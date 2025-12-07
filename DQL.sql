e68b664792384c2682846874293f1bdcd46ff33f
va259-class <va259@outlook.com>
2025-12-07 13:32:20 +0300

    subqueries, in, case, distinct, group by samples etc

--- DQL.sql
+++ DQL.sql
@@ -187,3 +187,136 @@ select s.CompanyName, count(0) as ProductCount from Suppliers s
 inner join Products p on s.SupplierID = p.SupplierID
 group by s.CompanyName
 order by 2 desc
+
+-- Tedarik edilip ürünleri en çok satýlan ve gelir elde edilen firmalar
+select top 5 s.CompanyName, sum(od.UnitPrice * od.Quantity) as Total 
+from Suppliers s
+inner join Products p on s.SupplierID = p.SupplierID
+inner join [Order Details] od on p.ProductID = od.ProductID
+group by s.CompanyName
+order by Total desc
+-- 1997 Aðustos ayýnda elde edilen toplam gelir
+select sum(od.UnitPrice * od.Quantity) as Total
+from Orders o
+inner join [Order Details] od on o.OrderID = od.OrderID
+where DATEPART(YEAR, o.OrderDate) = 1997 and
+	  DATEPART(MONTH, o.OrderDate) = 8 
+-- Adet bazýnda en çok satýlan 5 ürün 
+select top 5 p.ProductName, sum(od.Quantity) as Quantity 
+from Products p
+inner join [Order Details] od on p.ProductID = od.ProductID
+group by p.ProductName
+order by Quantity desc
+-- Tutar bazýnda en çok satýlan ürün
+select top 5 p.ProductName, sum(od.UnitPrice * od.Quantity) as Total 
+from Products p
+inner join [Order Details] od on p.ProductID = od.ProductID
+group by p.ProductName
+order by Total desc
+-- Ýçecek ve Tatlý kategorisindeki ürünler
+--Yöntem 1:
+select ProductName from Products 
+where CategoryID = 1 or CategoryID = 3
+-- Yöntem 2:
+select ProductName from Products 
+where CategoryID in (1,3)
+--Yöntem 3: SUB QUERY
+select ProductName from Products 
+where CategoryID in
+(
+	select CategoryID from Categories
+	where CategoryName in ('Beverages', 'Confections')
+)
+-- 1997 yýlýnda ortalama sipariþ tutarý
+select avg(od.UnitPrice * od.Quantity) as AverageSale from Orders o
+inner join [Order Details] od on o.OrderID = od.OrderID
+where datepart(year, o.OrderDate) = 1997
+-- 1997 yýlýnda aylýk gelir daðýlýmý
+select datepart(month, o.OrderDate) Month, avg(od.UnitPrice * od.Quantity) as AverageSale 
+from Orders o
+inner join [Order Details] od on o.OrderID = od.OrderID
+where datepart(year, o.OrderDate) = 1997
+group by datepart(month, o.OrderDate)
+order by 1
+-- 1997 2. çeyreðinde satýþ personellerinin performansý
+select e.FirstName, e.LastName, sum(od.UnitPrice * od.Quantity) as  Total 
+from Orders o
+inner join Employees e on o.EmployeeID = e.EmployeeID
+inner join [Order Details] od on o.OrderID = od.OrderID
+where datepart(year, o.OrderDate) = 1997 and datepart(month, OrderDate) in(4,5,6)
+group by e.FirstName, e.LastName
+order by 2 desc
+-- En az sipariþ verilen ay
+select top 1
+	datepart(year, o.OrderDate) as [Year], 
+	datepart(month, o.OrderDate) as [Month], 
+	sum(od.UnitPrice * od.Quantity) as Total 
+from Orders o
+inner join [Order Details] od on o.OrderID = od.OrderID
+group by datepart(year, o.OrderDate), datepart(month, o.OrderDate)
+order by Total asc
+-- 1998 Nisan ayýnda verilen sipariþleri teslim durumu
+select 
+	o.OrderID, 
+	case 
+		when ShippedDate is null then 'Teslim Edilmedi'
+		when ShippedDate is not null then 'Teslim Edildi' 
+	end as [Status]
+from Orders o
+where datepart(year, o.OrderDate) = 1998 and 
+	  datepart(month, o.OrderDate) = 4
+order by 2 desc
+-- Sipariþ durumuna göre ürün listesi
+select 
+	ProductName, 
+	case 
+		when UnitsOnOrder > 0 then 'Sevkiyat Bekliyor'
+		else '-'
+	end as [Status]
+from Products
+order by [Status] desc
+-- WHAT YOU MEASURE IS WHAT YOU GET
+-- Belirli bir ürünü alan müþterilerin listesi (Örnek 1 nolu ürün)
+
+select CompanyName, Phone from Customers
+where CustomerID in
+(
+	-- distinct ile tekrar eden satýrlar tekilleþtirildi
+	-- UYARI: DISTINCT çok kolonlu tablolarda pek tercih edilmemeli
+	select distinct o.CustomerID from Orders o
+	inner join [Order Details] od on o.OrderID = od.OrderID
+	where od.ProductID = 2
+)
+order by CompanyName
+
+-- YÖNTEM 2
+select CompanyName, Phone from Customers
+where CustomerID in
+(
+	-- distinct ile tekrar eden satýrlar tekilleþtirildi
+	-- UYARI: DISTINCT çok kolonlu tablolarda pek tercih edilmemeli
+	select distinct o.CustomerID from Orders o
+	inner join [Order Details] od on o.OrderID = od.OrderID
+	where od.ProductID = (
+		select top 1 ProductID from Products
+		where ProductName = 'Chang'
+	)
+)
+order by CompanyName
+
+-- 1997 yýlýnda indirimlerden kaynaklý kaybedilen ay bazlý gelir
+select 
+	datepart(month, o.OrderDate) as [Month],
+	round(sum(od.UnitPrice * od.Quantity * od.Discount), 2) as Discount
+from Orders o
+inner join [Order Details] od on o.OrderID = od.OrderID
+where datepart(year, o.OrderDate) = 1997 and od.Discount > 0
+group by datepart(month, o.OrderDate)
+order by [Month]
+
+-- personelin ülkelerine göre satýþ performans
+select e.Country, sum(od.UnitPrice * od.Quantity) as Total 
+from [Order Details] od
+inner join Orders o on od.OrderID = o.OrderID
+inner join Employees e on o.EmployeeID = e.EmployeeID
+group by e.Country
